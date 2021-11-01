2b3b197966624bbe19f4d26364e4e3348f233c82
Max Kalahur <kalahur@teamharbour.com>
2021-11-01 13:59:35 +0200

    Fix Queue

--- queue.c
+++ queue.c
@@ -3,153 +3,123 @@
 #include <string.h>
 
 typedef struct {
-    int val;             // 4 bites
-    void * nextNode;     // 8 bites
+    int val;
+    void * nextNode;
 } listNode;
 
-listNode * head = NULL;
-listNode * tail = NULL;
-int size = 0;
+typedef struct {
+    listNode * head;
+    listNode * tail;
+} queueStruct;
 
-listNode * createNode( int newVal );
-void enqueue( int newVal );
-int dequeue();
-void freeNode(listNode * head);
-void printList(listNode* tmpHead);
+queueStruct * queueInit();
+void enqueue( int newVal, queueStruct * queue );
+int dequeue( queueStruct * queue );
+listNode * createQueueNode( int newVal );
+void freeQueueNode(listNode * head);
 
 
-void queueInit() {
+queueStruct * queueInit() {
 
-    head = (listNode *) malloc(sizeof(listNode));
-    tail = (listNode *) malloc(sizeof(listNode));
-    
-    if (head == NULL || tail == NULL) {
+    queueStruct * queue;
+
+    queue = (queueStruct *) malloc(sizeof(queueStruct));
+    if (queue == NULL) {
         printf("out of memory error\n");
-		exit(1);
+        return NULL;
     }
+
+    return queue;
 }
 
-void enqueue( int newVal ) {
+void enqueue( int newVal, queueStruct * queue ) {
 	
-    listNode * newNode = createNode(newVal);
-    if( size == 0 ) {
-        queueInit();
-        head = newNode;
+    listNode * newNode = createQueueNode(newVal);
+
+    if( queue->head == NULL ) {
+        queue->head = (listNode *) malloc(sizeof(listNode));
+        if (queue->head == NULL) {
+            printf("out of memory error\n");
+        }
+        queue->head = newNode;
     }
-    tail->nextNode = newNode;
-    tail = newNode;
-    size++;
+
+    if (queue->tail == NULL) {
+        queue->tail = (listNode *) malloc(sizeof(listNode));
+        if (queue->tail == NULL) {
+            printf("out of memory error\n");
+        }
+    }
+    queue->tail->nextNode = newNode;
+    queue->tail = newNode;
 }
 
 
-int dequeue() {
+int dequeue( queueStruct * queue ) {
 
-    int valTmp = head->val;                 // tmp var
-    listNode * nextNode = head->nextNode;   // tmp var
+    int valTmp = queue->head->val;                 // tmp var
+    listNode * nextNode = queue->head->nextNode;   // tmp var
+    freeQueueNode(queue->head);                    // free memory of current head
+    queue->head = nextNode;                        // move head toward
 
-    freeNode(head);                         // free memore of current head
-    head = nextNode;                        // move head toward
-    size--;                                 // decrease queue size
-
-    if (head == NULL) {
+    if (queue->head == NULL) {
         printf("Queue is empty, bye bye\n");
-		exit(1);
+        return -1;
     }
-
     return valTmp;
 }
 
-listNode * createNode( int newVal ) {
+listNode * createQueueNode( int newVal ) {
     listNode * newNode = (listNode *) malloc (sizeof(listNode));
 
 	if (newNode == NULL) {
 		printf("out of memory error\n");
-		exit(1);
+        return NULL;
 	}
-
 	newNode->val = newVal;
 	newNode->nextNode = NULL;
 	return newNode;
 }
 
 
-void freeNode(listNode * head) {
-
-	free(head);
-	head = NULL;
-}
-
-void printList(listNode* tmpHead) {
-	while(tmpHead != NULL) {
-        printf("%d\t", tmpHead->val); 
-		tmpHead = tmpHead->nextNode;
-	}
-    printf("\n\n");
+void freeQueueNode(listNode * node) {
+	free(node);
+	node = NULL;
 }
 
 int main() {
 
-    enqueue(4);
-    printList(head);
+    /* Example of use: */
+    queueStruct * queue;
+    queue = queueInit();
 
-    enqueue(5);
-    printList(head);
-
-    enqueue(-2);
-    printList(head);
-
-    enqueue(0);
-    printList(head);
-
-    dequeue();
-    printList(head);
-
-    enqueue(99);
-    printList(head);
-
-    dequeue();
-    printList(head);
-
-    dequeue();
-    printList(head);
-
-    enqueue(33);
-    printList(head);
-
-    dequeue();
-    printList(head);
-
-    dequeue();
-    printList(head);
-
-    dequeue();
-    printList(head);
+    enqueue(4, queue);      printf("enqueue(4, queue);  // head - %d, tail - %d\n", queue->head->val, queue->tail->val);
+    enqueue(5, queue);      printf("enqueue(5, queue);  // head - %d, tail - %d\n", queue->head->val, queue->tail->val);
+    enqueue(-2, queue);     printf("enqueue(-2, queue); // head - %d, tail - %d\n", queue->head->val, queue->tail->val);
+    enqueue(0, queue);      printf("enqueue(0, queue);  // head - %d, tail - %d\n", queue->head->val, queue->tail->val);
+    dequeue(queue);         printf("dequeue(queue);     // head - %d, tail - %d\n", queue->head->val, queue->tail->val);
+    enqueue(99, queue);     printf("enqueue(99, queue); // head - %d, tail - %d\n", queue->head->val, queue->tail->val);
+    dequeue(queue);         printf("dequeue(queue);     // head - %d, tail - %d\n", queue->head->val, queue->tail->val);
+    dequeue(queue);         printf("dequeue(queue);     // head - %d, tail - %d\n", queue->head->val, queue->tail->val);
+    enqueue(33, queue);     printf("enqueue(33, queue); // head - %d, tail - %d\n", queue->head->val, queue->tail->val);
+    dequeue(queue);         printf("dequeue(queue);     // head - %d, tail - %d\n", queue->head->val, queue->tail->val);
+    dequeue(queue);         printf("dequeue(queue);     // head - %d, tail - %d\n", queue->head->val, queue->tail->val);
+    dequeue(queue);         
 
 /*
 Output:
 
-4
-
-4       5
-
-4       5       -2
-
-4       5       -2      0
-
-5       -2      0
-
-5       -2      0       99
-
--2      0       99
-
-0       99
-
-0       99      33
-
-99      33
-
-33
-
+enqueue(4, queue);  // head - 4, tail - 4
+enqueue(5, queue);  // head - 4, tail - 5
+enqueue(-2, queue); // head - 4, tail - -2
+enqueue(0, queue);  // head - 4, tail - 0
+dequeue(queue);     // head - 5, tail - 0
+enqueue(99, queue); // head - 5, tail - 99
+dequeue(queue);     // head - -2, tail - 99
+dequeue(queue);     // head - 0, tail - 99
+enqueue(33, queue); // head - 0, tail - 33
+dequeue(queue);     // head - 99, tail - 33
+dequeue(queue);     // head - 33, tail - 33
 Queue is empty, bye bye
 */
 
